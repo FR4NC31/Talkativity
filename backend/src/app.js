ebdabc9770ea804b525c89ba69110b6c1275fa45
FR4NC Ortega <francisedgard16@gmail.com>
2026-06-14 11:54:18 +0800

    deploying render

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import "dotenv/config"
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = process.env.PORT 
 
 app.listen(PORT,  () => console.log(`Server running on port ${PORT}`))
+
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err))
